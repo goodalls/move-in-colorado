26ba8688eb154cc825a1f769c7b4f039b2c1ecd1
Jeff Goodall <goodallhj@gmail.com>
2018-03-24 08:52:55 -0600

    setup files

---
+++ src/components/Map/Map.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { key } from '../../key';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import ReactDOM from 'react-dom';
+
+export class MapContainer extends React.Component {
+  componentDidMount() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    if (this.props && this.props.google) {
+      // google is available
+      const { google } = this.props;
+      const maps = google.maps;
+
+      const mapRef = this.refs.map;
+      const node = ReactDOM.findDOMNode(mapRef);
+
+      let zoom = 14;
+      let lat = 37.774929;
+      let lng = -122.419416;
+      const center = new maps.LatLng(lat, lng);
+      const mapConfig = Object.assign(
+        {},
+        {
+          center: center,
+          zoom: zoom
+        }
+      );
+      this.map = new maps.Map(node, mapConfig);
+    }
+    // ...
+  }
+
+  render() {
+    const style = {
+      width: '100vw',
+      height: '100vh'
+    };
+    if (!this.props.loaded) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div style={style}>
+        <Map google={this.props.google} />
+      </div>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: key
+})(MapContainer);
